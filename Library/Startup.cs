9057eaa2486be390fd599d77620b55a50a9fe67e
Barry Sturgeon <bazsturgeon3017@gmail.com>
2019-02-16 17:30:30 +0200

    Setting up connections

--- Library/Startup.cs
+++ Library/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibraryData;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,14 +10,20 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace Library
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -24,20 +31,27 @@ namespace Library
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            /*
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            */
+            // add framework services
+            services.AddMvc();
+
+            services.AddDbContext<LibraryContext>( options 
+                => options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection"))) 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Middleware
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
---
+++ LibraryData/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LibraryData
+{
+    public class LibraryContext: DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Patron> Patrons { get; set;  }
+
+
+    }
+}
